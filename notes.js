4058b8d4be11e103ab6e2f3036db7364ff5d4433
Micheal Garcia <garciamicheal0@outlook.com>
2022-02-21 10:05:03 -0800

    Create notes.js

---
+++ notes.js
@@ -0,0 +1,34 @@
+// The 7 Data Types
+
+// There are 6 *Primitive data types.
+// Primitive data types are the building block for all kother data types
+// They are the most basic form of data.
+
+// Primitives.
+null //null means nothing, absence of value
+undefined // a variable with no assigned value
+// Some might say that null and undefined is the same, but 
+// one is the value of nothing, the other has no defined value.
+true false // These are boolean values, true or false
+123456 // number, its numbers
+"Strings" // Strings is text or seen as text (string)
+// symbol // is made with a built in object, and it is gauranteed
+// to be unique. i.e. if you declare two variables with the same
+// symbol, they will not equal each other.
+// Often used to add unique property keys to an object that wont
+// collide with keys any other code might add to an object
+
+// Complex data type
+// Object
+// It is used to store various keyed collections and complex entities.
+// Key: Value pairs, these are essentially objects.
+// Say you have a car object, its color: red, speed: 'fast', etc.
+
+
+// Declaring Variables
+
+// var, let, const
+// let and const are more complicated on the topic of scope.
+// so just understand that they are all used for declarations of variables.
+// var was the only way to declare variables in the past (pre 2015)
+// you can't reassign a const variable.
